9fadaf920d2713acf0c89e6ef915c0044bc38326
kurt <developer@moyst.ca>
2025-05-23 17:29:02 -0230

    Removed ResponseContent.php (no longer in use)

--- lib/netPhramework/rendering/Display.php
+++ lib/netPhramework/rendering/Display.php
@@ -8,11 +8,11 @@ use netPhramework\core\ResponseCode;
 
 readonly class Display implements Response
 {
-	public function __construct(private Viewable $content, private ResponseCode $code) {}
+	public function __construct(private Viewable $content,
+								private ResponseCode $code) {}
 
 	public function deliver(Responder $responder): void
 	{
 		$responder->display($this->content, $this->code);
 	}
-
 }
