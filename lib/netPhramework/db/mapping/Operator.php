d424da4057971bc279890b744242d5da43843461
kurt <developer@moyst.ca>
2025-05-28 21:37:20 -0230

    added some Operators and InvalidComparator.php exception class

--- lib/netPhramework/db/mapping/Operator.php
+++ lib/netPhramework/db/mapping/Operator.php
@@ -3,6 +3,7 @@
 namespace netPhramework\db\mapping;
 
 use netPhramework\common\EnumToArray;
+use netPhramework\db\exceptions\InvalidComparator;
 
 enum Operator:string
 {
@@ -10,16 +11,50 @@ enum Operator:string
 
 	case EQUAL = 'EQUALS';
 	case CONTAINS = 'CONTAINS';
-	case STARTS_WITH = 'STARTS_WITH';
+	case STARTS_WITH = 'STARTS WITH';
+	case GREATER_THAN = 'GREATER THAN';
+	case GREATER_OR_EQUAL = "GREATER OR EQUAL";
+	case LESS_THAN = 'LESS THAN';
+	case LESS_OR_EQUAL = 'LESS OR EQUAL';
 
-	public function check(string $haystack, string $needle):bool
+	/**
+	 * @param string $storedValue
+	 * @param string $formValue
+	 * @return bool
+	 * @throws InvalidComparator
+	 */
+	public function check(string $storedValue, string $formValue):bool
 	{
 		if($this === self::CONTAINS)
-			return stristr($haystack, $needle);
+			return stristr($storedValue, $formValue);
 		elseif($this === self::EQUAL)
-			return $haystack == $needle;
+			return $storedValue == $formValue;
 		elseif($this === self::STARTS_WITH)
-			return str_starts_with($haystack, $needle);
+			return str_starts_with($storedValue, $formValue);
+		elseif($this === self::GREATER_THAN)
+		{
+			if(!is_numeric($formValue) || !is_numeric($storedValue))
+				throw new InvalidComparator("Invalid comparator for field");
+			return $storedValue > $formValue;
+		}
+		elseif($this === self::LESS_THAN)
+		{
+			if(!is_numeric($formValue) || !is_numeric($storedValue))
+				throw new InvalidComparator("Invalid comparator for field");
+			return $storedValue < $formValue;
+		}
+		elseif($this === self::GREATER_OR_EQUAL)
+		{
+			if(!is_numeric($formValue) || !is_numeric($storedValue))
+				throw new InvalidComparator("Invalid comparator for field");
+			return $storedValue >= $formValue;
+		}
+		elseif($this === self::LESS_OR_EQUAL)
+		{
+			if(!is_numeric($formValue) || !is_numeric($storedValue))
+				throw new InvalidComparator("Invalid comparator for field");
+			return $storedValue <= $formValue;
+		}
 		else exit(1);
 	}
 }
