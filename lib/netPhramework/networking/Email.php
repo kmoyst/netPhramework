cf023b51632b6f7ca6d34bb2567ac4e3828a6ae7
kurt <developer@moyst.ca>
2025-07-06 15:09:58 -0230

    Simplified Email.php to use only plain text for now.

--- lib/netPhramework/networking/Email.php
+++ lib/netPhramework/networking/Email.php
@@ -12,12 +12,81 @@ class Email
 	private string $senderName;
 	private string $subject;
 	private string $message;
+	private string $boundary;
 	private StreamSocket $socket;
 
 	public function __construct(
 		private readonly string $smtpServer = 'ssl://mail.moyst.ca:465',
 		private readonly string $sendingServer = 'moyst.ca',
-		private readonly string $charset = 'us-ascii') {}
+		private readonly string $charset = 'UTF-8'
+	)
+	{
+		$this->boundary = md5(uniqid());
+	}
+
+	/**
+	 * @return $this
+	 * @throws EmailException
+	 * @throws Exception
+	 * @throws StreamSocketException
+	 */
+	public function send():self
+	{
+		if(!$this->isPrepared())
+			throw new Exception("Tried to send email unprepared email");
+		$this->socket = new StreamSocket($this->smtpServer);
+		try {
+			$this->verify(SmtpResponseCode::SERVICE_READY,
+				$this->socket->open()->readResponseCode());
+			$this->verify(SmtpResponseCode::OK, $this->socket
+				->write("helo $this->sendingServer")->readResponseCode());
+			$this->verify(SmtpResponseCode::OK, $this->socket
+				->write("mail from: $this->sender")->readResponseCode());
+			$this->verify(SmtpResponseCode::OK, $this->socket
+				->write("rcpt to: $this->recipient")->readResponseCode());
+			$this->verify(SmtpResponseCode::START_MAIL_INPUT, $this->socket
+				->write("data")->readResponseCode())
+			;
+			$this->writePlainText()
+			;
+			$this->verify(SmtpResponseCode::OK, $this->socket
+				->write('.')->readResponseCode());
+			$this->verify(SmtpResponseCode::GOODBYE, $this->socket
+				->write('quit')->readResponseCode());
+		} catch (SmtpException $e) {
+			$msg = $e->getMessage() . ', ' . $this->socket->getLastMessage();
+			throw new SmtpException(trim($msg,', '));
+		} finally {
+			$this->socket->close();
+		}
+		return $this;
+	}
+
+	private function writePlainText():void
+	{
+		$this->socket
+			->write("From: " . $this->resolveSenderName())
+			->write("To: " . $this->resolveRecipientName())
+			->write("Subject: $this->subject")
+			->write("MIME-Version: 1.0")
+			->write("Content-Type: text/plain; charset=$this->charset")
+			->write("Content-Transfer-Encoding: 8bit")
+			->write('')
+			->write($this->message ?? '')
+			;
+	}
+
+	/**
+	 * @param SmtpResponseCode $code
+	 * @param int|null $responseCode
+	 * @return void
+	 * @throws SmtpException
+	 */
+	private function verify(SmtpResponseCode $code, ?int $responseCode):void
+	{
+		if(!$code->test($responseCode))
+			throw new SmtpException("Expected $code->value");
+	}
 
 	private function isPrepared():bool
 	{
@@ -42,68 +111,7 @@ class Email
 			return $this->recipient;
 	}
 
-	/**
-	 * @return $this
-	 * @throws EmailException
-	 * @throws Exception
-	 * @throws StreamSocketException
-	 */
-	public function send():self
-	{
-		if(!$this->isPrepared())
-			throw new Exception("Tried to send email unprepared email");
-		$boundary = md5('random');
-		$textType = 'text/html';
-		$this->socket = new StreamSocket($this->smtpServer);
-		try {
-			$this->verify(SmtpResponseCode::SERVICE_READY,
-				$this->socket->open()->readResponseCode());
-			$this->verify(SmtpResponseCode::OK, $this->socket
-				->write("helo $this->sendingServer")->readResponseCode());
-			$this->verify(SmtpResponseCode::OK, $this->socket
-				->write("mail from: $this->sender")->readResponseCode());
-			$this->verify(SmtpResponseCode::OK, $this->socket
-				->write("rcpt to: $this->recipient")->readResponseCode());
-			$this->verify(SmtpResponseCode::START_MAIL_INPUT, $this->socket
-				->write("data")->readResponseCode());
-			$this->verify(SmtpResponseCode::OK, $this->socket
-				->write("From: " . $this->resolveSenderName())
-				->write("To: " . $this->resolveRecipientName())
-				->write("Subject: $this->subject")
-				->write("MIME-Version: 1.0")
-				->write("Content-Type: multipart/mixed; boundary=\"$boundary\"")
-				->write('')
-				->write("--$boundary")
-				->write("Content-Type: $textType; charset=$this->charset")
-				->write('')
-				->write($this->message ?? '')
-				->write('')
-				->write("--$boundary--")
-				->write('')
-				->write('.')
-				->readResponseCode());
-			$this->verify(SmtpResponseCode::GOODBYE, $this->socket
-				->write('quit')->readResponseCode());
-		} catch (SmtpException $e) {
-			$msg = $e->getMessage() . ', ' . $this->socket->getLastMessage();
-			throw new SmtpException(trim($msg,', '));
-		} finally {
-			$this->socket->close();
-		}
-		return $this;
-	}
-
-	/**
-	 * @param SmtpResponseCode $code
-	 * @param int|null $responseCode
-	 * @return void
-	 * @throws SmtpException
-	 */
-	private function verify(SmtpResponseCode $code, ?int $responseCode):void
-	{
-		if(!$code->test($responseCode))
-			throw new SmtpException("Expected $code->value");
-	}
+	// SETTER METHODS //
 
 	public function setRecipient(string $recipient): self
 	{
