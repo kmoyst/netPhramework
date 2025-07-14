076e4c7e6f987497ff7d44f35a089c40cf941b2f
kurt <developer@moyst.ca>
2025-07-14 13:29:30 -0230

    made readonly properties in ImmutableView.php public

--- lib/netPhramework/rendering/ImmutableView.php
+++ lib/netPhramework/rendering/ImmutableView.php
@@ -5,8 +5,8 @@ namespace netPhramework\rendering;
 class ImmutableView extends Viewable
 {
 	public function __construct(
-		private readonly string $templateName,
-		private readonly iterable $variables) {}
+		public readonly string $templateName,
+		public readonly iterable $variables) {}
 
 	public function getTemplateName(): string
 	{
