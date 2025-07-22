657256c3f083a0cf6251328ec418c3c1383d2e5f
kurt <developer@moyst.ca>
2025-07-22 06:21:45 -0230

    removed unnecessary method configureActiveNode

--- lib/netPhramework/core/Application.php
+++ lib/netPhramework/core/Application.php
@@ -10,14 +10,12 @@ abstract class Application
 	/**
 	 * @param Directory $root
 	 * @return void
-	 * @throws Exception
 	 */
 	abstract public function configurePassiveNode(Directory $root):void;
 
 	/**
 	 * @param Directory $root
 	 * @return void
-	 * @throws Exception
 	 */
-	abstract public function configureActiveNode(Directory $root):void;
+	public function configureActiveNode(Directory $root):void {}
 }
